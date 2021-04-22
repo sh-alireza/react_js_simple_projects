58b876f2547f17b215d478d372a9d8a21f62215c
alireza <a.sharifi45@yahoo.com>
2021-04-22 20:29:30 +0430

    default

--- src/APPs/snake-game/components/items.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./items.css";
-const items = () => {
-    return (
-        <div/>
-    );
-}
-export default items;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from "./APPs/snake-game/App";
-
-
-ReactDOM.render(<App/>,document.getElementById('root'));
